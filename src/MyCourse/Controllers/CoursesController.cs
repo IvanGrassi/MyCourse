87e7fc32fac4188f10c64c3f97e6bdf32d725d75
Ivan Grassi <g.ivan.1994@gmail.com>
2020-03-26 10:38:38 +0100

    Aggiornamento View (causa problematica)

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -6,12 +6,12 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()        //Courses
         {
-            return Content("Sono index");
+            return View();      //va a cercare una view chiamata Index.cshtml in courses
         }
 
         public IActionResult Detail(string id)  //Detail/5
         {
-            return Content($"Sono detail, ho ricevuto l'id {id}");
+            return View();
         }
     }
 }
