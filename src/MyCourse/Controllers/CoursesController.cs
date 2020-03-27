98dbc9d335304eef38a3043484e68fc439a54c5d
Ivan Grassi <g.ivan.1994@gmail.com>
2020-03-27 14:36:39 +0100

    Inizio sviluppo Model (sez 8)

--- src/MyCourse/Controllers/CoursesController.cs
+++ src/MyCourse/Controllers/CoursesController.cs
@@ -1,4 +1,7 @@
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using MyCourse.Models.Services.Application;
+using MyCourse.Models.ViewModels;
 
 namespace MyCourse.Controllers
 {
@@ -6,7 +9,10 @@ namespace MyCourse.Controllers
     {
         public IActionResult Index()        //Courses
         {
-            return View();      //va a cercare una view chiamata Index.cshtml in courses
+            //creiamo un istanza di CourseService, invochiamo il suo metodo GetCourses per ottenere l'elenco dei corsi
+            var courseService = new CourseService();
+            List<CourseViewModel> courses = courseService.GetServices();
+            return View(courses);      //va a cercare una view chiamata Index.cshtml in courses e gli passa i dati ottenuti tramite metodo
         }
 
         public IActionResult Detail(string id)  //Detail/5
