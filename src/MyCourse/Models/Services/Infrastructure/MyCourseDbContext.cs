ddf4c7a70d08b12dc4a20f557c7051d466c85d0f
Ivan Grassi <g.ivan.1994@gmail.com>
2020-05-29 14:39:53 +0200

    Sezione 17: Front End reworked

--- src/MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
+++ src/MyCourse/Models/Services/Infrastructure/MyCourseDbContext.cs
@@ -26,6 +26,8 @@ namespace MyCourse.Models.Services.Infrastructure
                 entity.HasKey(course => course.Id);    //indica la PK, superfluo se la proprietà si chiama Id oppure CorsesId
                 //entity.HasKey(course => new object{ course.Id, course.Author}) //SE abbiamo piu di una PK in tabella
 
+                //PER LE MIGRATION (per gli indici)
+                entity.HasIndex(course => course.Title).IsUnique();
                 //concorrenza ottimistica
                 entity.Property(course => course.RowVersion).IsRowVersion();
 
